6108e04b5c5949f7b615d08dfaec7e47310cca11
Marcin Kaczmarek <mkmarol@gmail.com>
2020-07-02 18:39:52 +0200

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@
   console.log('links:', links);
 });*/
 
-const titleClickHandler = function(){
+const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event)
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -15,9 +15,12 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts .post.active');
 
