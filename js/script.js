bef1e121a344f7216069cb0b3b65fd11b7fca9fc
Marcin Kaczmarek <mkmarol@gmail.com>
2020-07-07 22:08:43 +0200

    prevent default and add const clickedElement

--- js/script.js
+++ js/script.js
@@ -159,7 +159,11 @@ generateTags();
 function tagClickHandler(event){
   /* prevent default action for this event */
 
+  event.preventDefault();
+
   /* make new constant named "clickedElement" and give it the value of "this" */
+
+  const clickedElement = this;
 
   /* make a new constant "href" and read the attribute "href" of the clicked element */
 
